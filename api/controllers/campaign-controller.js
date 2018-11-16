af04ce1a5754a466ef577054e8c24a879ae76de3
Robert Waggott <robert.waggott@gmail.com>
2018-11-16 11:47:58 +0000

    -- fixing build error

--- api/controllers/campaign-controller.js
+++ api/controllers/campaign-controller.js
@@ -4,14 +4,10 @@ const CampaignsService = require("../services/campaigns-service");
 module.exports = class CampaignsController extends BaseController {
     async get(ctx) {
         let id = ctx.params.campaignId;
-<<<<<<< HEAD
         
         let campaignService = new CampaignsService();
 
         let campaign = await campaignService.get(id);
-=======
-        let campaign = new CampaignsService().get(id);
->>>>>>> c4464030a7ad6192144e7946646cdb9a6f43cc43
 
         ctx.body = campaign;
     }
