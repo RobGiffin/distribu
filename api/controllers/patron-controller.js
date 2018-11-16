d49b932fba3aea613f50c01b13cc890b35097122
Robert Waggott <robert.waggott@gmail.com>
2018-11-16 10:37:23 +0000

    -- setting up to use stripe to take payments

--- api/controllers/patron-controller.js
+++ api/controllers/patron-controller.js
@@ -1,11 +1,30 @@
 const BaseController = require("./base-controller");
 const CampaignsService = require("../services/campaigns-service");
 
+const keyPublishable = "pk_test_uP4xO5fMSTcUqez9MQ67b60X";
+const keySecret = "sk_test_YrQjWTPsU80XFOK9cI4bUXM6";
+const stripe = require("stripe")(keySecret);
+
 module.exports = class PatronController extends BaseController {
     async supportCampaign(ctx) {
         let campaignId = ctx.params.campaignId;
+        let campaign = new CampaignsService().get(campaignId);
         let patronId = ctx.params.patronId;
+        let stripeToken = req.body.stripeToken;
+        let amount = req.body.amount;
 
-        
+        stripe.charges.create({
+            amount: amount,
+            currency: "gbp",
+            description: `Distribu - ${campaign.name}`,
+            source: stripeToken,
+        }, (err, charge) => {
+            if (err) { 
+                console.warn(err) 
+            } 
+            else {
+                res.status(200).send(charge)
+            }
+        });              
     }
 };
